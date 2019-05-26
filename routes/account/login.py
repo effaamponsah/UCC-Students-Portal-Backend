8b48d81a30a7963b54b62727e92bfcb5c33ad4cd
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-05-26 07:19:08 +0100

    login complete

--- routes/account/login.py
+++ routes/account/login.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, request, jsonify, url_for
 import pymysql
-
+from utils.user import User
+import jwt
+import datetime
+import hashlib
 def _login():
     if request.method == "GET":
         return jsonify(
@@ -8,10 +11,27 @@ def _login():
         )
     else:
         body = request.get_json()
-        if body["indexnumber"] == "PS/CSC/15/0004" and body["password"] == "effa":
-            # return jsonify({"Success": True})
-            return redirect(url_for("index"))
-
+        index = (body["indexnumber"]).upper()
+        response = User(index, body["password"])
+        if None in response:
+            return jsonify(
+                Success=False, Warning="Student not found. Enter correct Credentials"
+            )
         else:
-            return jsonify(Success=False, Warning=" Please provide correct credentials")
-        # return jsonify({"Success": True, "Message": "Welcome " + response["username"]})
+            auth_token = jwt.encode(
+                {
+                    "indexnumber": body["indexnumber"],
+                    "id": response[0]['id'],
+                    "exp": datetime.datetime.utcnow() + datetime.timedelta(days=5),
+                },
+                "226905605219861890656256635479281220067966797385942583903537",  # Use a more secured algorithm
+                # i used a random to get 200 random integers
+            )
+            return jsonify(Success=True, Student=response[0], Token=auth_token.decode())
+        # if body["indexnumber"] == "PS/CSC/15/0004" and body["password"] == "effa":
+        #     # return jsonify({"Success": True})
+        #     return redirect(url_for("index"))
+
+        # else:
+        #     return jsonify(Success=False, Warning=" Please provide correct credentials")
+        # # return jsonify({"Success": True, "Message": "Welcome " + response["username"]})
